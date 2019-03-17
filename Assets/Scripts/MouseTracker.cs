13256b13272792edae65c8b223505711049afae3
Michał Domino <micdom9@gmail.com>
2019-03-17 16:50:11 +0100

    Dodano celowanie w górę i w dół.

--- Assets/Scripts/MouseTracker.cs
+++ Assets/Scripts/MouseTracker.cs
@@ -13,24 +13,11 @@ public class MouseTracker : MonoBehaviour
     protected void TrackMouse()
     {
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        //Vector3 vector = new Vector3(0.0f, 0.0f, 0.);
-        RaycastHit[] hit;
+        
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
-        //if (Physics.Raycast(cameraRay, out float rayLenght))
-        //{
-        //    pointToLook = cameraRay.GetPoint(rayLenght);
-        //}
-        if (Physics.Raycast(cameraRay, Mathf.Infinity))
+        if (groundPlane.Raycast(cameraRay, out float rayLenght))
         {
-            Debug.DrawLine(cameraRay, Color.black);
-            Debug.DrawRay(cameraRay, Color.black,);
-            Debug.
-            Debug.DrawRay()
-            hit = Physics.RaycastAll(cameraRay, Mathf.Infinity);
-            //pointToLook = cameraRay.GetPoint(hit[0].point);
-            pointToLook = hit[0].point;
+            pointToLook = cameraRay.GetPoint(rayLenght);
         }
-
-
     }
 }
