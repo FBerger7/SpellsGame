a37698f21550ed6d49f0e49f90ccef4f62e45010
Michał Domino <micdom9@gmail.com>
2019-03-21 14:45:32 +0100

    Dodano przywoływanie obiektu przy dłoni.

--- Assets/Scripts/Summon.cs
+++ Assets/Scripts/Summon.cs
@@ -4,33 +4,32 @@ using UnityEngine;
 
 public class Summon : MouseTracker
 {
-    public Projectile projectile;
+    public GameObject objectToSummon;
     public KeyCode fireKey;
-    public float projectileSpeed;
     public float attackSpeed;
-    public float projectileLifeSpawn;
 
     public Transform firePoint;
 
     private float _attackCooldown;
 
-    // Start is called before the first frame update
-    void Start()
+    // Update is called once per frame
+    void Update()
     {
         _attackCooldown -= Time.deltaTime;
         if (Input.GetKey(fireKey))
         {
             PerformAttack();
         }
+
     }
 
-    // Update is called once per frame
     private void PerformAttack()
     {
         if (_attackCooldown <= 0)
         {
             _attackCooldown = attackSpeed;
-            Projectile newProjectile = Instantiate(projectile, pointToLook, firePoint.rotation) as Projectile;
+            Vector3 vector3 = new Vector3(pointToLook.x, pointToLook.y, pointToLook.z);
+            GameObject newProjectile = Instantiate(objectToSummon, transform.position, transform.rotation) as GameObject;
         }
     }
 }
