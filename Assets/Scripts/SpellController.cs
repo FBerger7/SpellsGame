346b22da180714e2de60b2666a8a5a3748480b91
Michał Domino <micdom9@gmail.com>
2019-03-24 13:41:28 +0100

    Dodano SpellController.

---
+++ Assets/Scripts/SpellController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellController : MonoBehaviour
+{
+    public BaseSpell baseSpell;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentSpell = new BasicAttack();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
