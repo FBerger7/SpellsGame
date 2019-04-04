12504d05c94eb1158af8d5a719ea774aaf73b826
Michał Domino <micdom9@gmail.com>
2019-04-04 17:44:00 +0200

    Dodano przełączanie pomiędzy parami spelli.

--- Assets/Scripts/OffensiveSpellsModel.cs
+++ Assets/Scripts/OffensiveSpellsModel.cs
@@ -7,16 +7,15 @@ public class OffensiveSpellsModel : MonoBehaviour
     public const int BASIC_SPELL = 0;
     public const int SUMMON = 1;
 
-    public List<BaseSpell> offesiveSpells;
+    public List<BaseSpell> offensiveSpells;
 
     // Start is called before the first frame update
     void Start()
     {
-        offesiveSpells = new List<BaseSpell>();
-        offesiveSpells.Add(gameObject.GetComponent<BasicAttack>());
-        offesiveSpells.Add(gameObject.GetComponent<Summon>());
+        offensiveSpells = new List<BaseSpell>();
+        offensiveSpells.Add(gameObject.GetComponent<BasicAttack>());
+        offensiveSpells.Add(gameObject.GetComponent<Summon>());
     }
-
 
     // Update is called once per frame
     void Update()
--- Assets/Scripts/SpellController.cs
+++ Assets/Scripts/SpellController.cs
@@ -9,10 +9,7 @@ public class SpellController : MonoBehaviour
     public const int BASIC_SPELL = 0;
     public const int SUMMON = 1;
 
-    public List<BaseSpell> offensiveSpells;
 
-    public KeyCode basicAttackActivationKey;
-    public KeyCode summonActivationKey;
 
     public KeyCode leftSpellKey;
     public KeyCode rightSpellKey;
@@ -20,77 +17,94 @@ public class SpellController : MonoBehaviour
 
     private List<Tuple<int, int>> _spellQueue;
     private int _currentPair;
-
-    private Dictionary<KeyCode, BaseSpell> spells;
+    private List<BaseSpell> _offensiveSpells;
     private float _remainingActivationCooldown = ACTIVATION_COOLDOWN;
-    private KeyCode _currentSpell;
-    //private OffensiveSpellsModel offensiveSpells;
+
+
+    // Może do tego wrócimy (nie usuwać jeszcze)
+    //public KeyCode basicAttackActivationKey;
+    //public KeyCode summonActivationKey;
+    //private Dictionary<KeyCode, BaseSpell> spells;
+    //private KeyCode _currentSpell;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        offensiveSpells = new List<BaseSpell>();
-        offensiveSpells.Add(gameObject.GetComponent<BasicAttack>());
-        offensiveSpells.Add(gameObject.GetComponent<Summon>());
         _spellQueue = new List<Tuple<int, int>>();
-        //offensiveSpells = gameObject.GetComponent<OffensiveSpellsModel>();
+        _offensiveSpells = gameObject.GetComponent<OffensiveSpellsModel>().offensiveSpells;
         // inicjalizacja listy spelli tylko do testów, potem tego nie będzie
+        // ------------------------------------------------------------------
         _spellQueue.Add(new Tuple<int, int>(OffensiveSpellsModel.BASIC_SPELL, OffensiveSpellsModel.SUMMON));
         _spellQueue.Add(new Tuple<int, int>(OffensiveSpellsModel.SUMMON, OffensiveSpellsModel.BASIC_SPELL));
-        //
+        // ------------------------------------------------------------------
         _currentPair = 0;
-        _currentSpell = KeyCode.None;
-        spells = new Dictionary<KeyCode, BaseSpell>();
-        AddSpells();
+
+        // Może do tego wrócimy (nie usuwać jeszcze)
+        //_currentSpell = KeyCode.None;
+        //spells = new Dictionary<KeyCode, BaseSpell>();
+        //AddSpells();
     }
 
     // Update is called once per frame
     void Update()
     {
         _remainingActivationCooldown -= Time.deltaTime;
-        if (Input.GetKey(basicAttackActivationKey))
+
+        if (Input.GetKey(changeSpellKey))
         {
-            ManageSpells(basicAttackActivationKey);
+            if (_remainingActivationCooldown <= 0)
+            {
+                _remainingActivationCooldown = ACTIVATION_COOLDOWN;
+                _currentPair = (_currentPair + 1) % _spellQueue.Count;
+            }
         }
         if (Input.GetKey(leftSpellKey))
         {
-            offensiveSpells[_spellQueue[0].Item1].PerformAttack();
-            //_spellQueue[0].Item1.PerformAttack();
+            _offensiveSpells[_spellQueue[0].Item1].PerformAttack();
         }
         if (Input.GetKey(rightSpellKey))
         {
-            offensiveSpells[_spellQueue[0].Item2].PerformAttack();
+            _offensiveSpells[_spellQueue[0].Item2].PerformAttack();
         }
 
-        if (Input.GetKey(summonActivationKey))
-        {
-            ManageSpells(summonActivationKey);
-        }
+        // Może do tego wrócimy (nie usuwać jeszcze)
+        //if (Input.GetKey(basicAttackActivationKey))
+        //{
+        //    ManageSpells(basicAttackActivationKey);
+        //}
+        //if (Input.GetKey(summonActivationKey))
+        //{
+        //    ManageSpells(summonActivationKey);
+        //}
     }
 
-    private void AddSpells()
-    {
-        spells.Add(basicAttackActivationKey, gameObject.GetComponent<BasicAttack>());
-        spells.Add(summonActivationKey, gameObject.GetComponent<Summon>());
-    }
+    // Może do tego wrócimy (nie usuwać jeszcze)
+    //private void AddSpells()
+    //{
+    //    spells.Add(basicAttackActivationKey, gameObject.GetComponent<BasicAttack>());
+    //    spells.Add(summonActivationKey, gameObject.GetComponent<Summon>());
+    //}
 
-    private void ManageSpells(KeyCode chosenSpell)
-    {
-        if (_remainingActivationCooldown <= 0)
-        {
-            _remainingActivationCooldown = ACTIVATION_COOLDOWN;
-            if (_currentSpell == chosenSpell)
-            {
-                spells[_currentSpell].deactivate();
-                _currentSpell = KeyCode.None;
-                return;
-            }
-            if (_currentSpell != KeyCode.None)
-            {
-                spells[_currentSpell].deactivate();
-            }
-            spells[chosenSpell].activate();
-            _currentSpell = chosenSpell;
-        }
-    }
+
+    //Może do tego wrócimy(nie usuwać jeszcze)
+    //private void ManageSpells(KeyCode chosenSpell)
+    //{
+    //    if (_remainingActivationCooldown <= 0)
+    //    {
+    //        _remainingActivationCooldown = ACTIVATION_COOLDOWN;
+    //        if (_currentSpell == chosenSpell)
+    //        {
+    //            spells[_currentSpell].deactivate();
+    //            _currentSpell = KeyCode.None;
+    //            return;
+    //        }
+    //        if (_currentSpell != KeyCode.None)
+    //        {
+    //            spells[_currentSpell].deactivate();
+    //        }
+    //        spells[chosenSpell].activate();
+    //        _currentSpell = chosenSpell;
+    //    }
+    //}
 }
