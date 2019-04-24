f941a151c350907d2523f44b3ab94dd3453b3ca3
Mirosław Ściebura (mirosci698) <mirosci698@student.polsl.pl>
2019-04-24 16:12:33 +0200

    Version 0.1

--- Assets/Scripts/CharacterInterface.cs
+++ Assets/Scripts/CharacterInterface.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 public class CharacterInterface : MonoBehaviour
 {
-
     private int healthPoints;
 
     private int healingSpell;
@@ -19,32 +18,63 @@ public class CharacterInterface : MonoBehaviour
 
     public Image healthPointsTexture;
 
+    public Image mainSpellFirstTexture;
+
+    public Image mainSpellSecondTexture;
+
+    public Image healingSpellTexture;
+
+    public Image parrySpellTexture;
+
+    public Image mobileSpellTexture;
+
     public Text healthPointsText;
 
     public Sprite[] healthSprites;
 
+    public Sprite[] mainSpellSprites;
+
+    public Sprite[] healingSpellSprites;
+
+    public Sprite[] mobileSpellSprites;
+
+    public Sprite[] parrySpellSprites;
+
     public void setHealthPoints(int number) {
-        healthPoints = number;
+        if (number > -1 && number < 101)
+            healthPoints = number;
+        healthPointsTexture.sprite = healthSprites[getIndexOfSprite()];
+        healthPointsText.text = "HP: " + healthPoints + "%";
     }
 
     public void setHealingSpell(int number) {
-        healingSpell = number;
+        if (number > -1 && number < healingSpellSprites.Length)
+            healingSpell = number;
+        healingSpellTexture.sprite = healingSpellSprites[healingSpell];
     }
 
     public void setMobileSpell(int number) {
-        mobileSpell = number;
+        if (number > -1 && number < mobileSpellSprites.Length)
+            mobileSpell = number;
+        mobileSpellTexture.sprite = mobileSpellSprites[mobileSpell];
     }
 
     public void setParrySpell(int number) {
-        parrySpell = number;
+        if (number > -1 && number < parrySpellSprites.Length)
+            parrySpell = number;
+        parrySpellTexture.sprite = parrySpellSprites[parrySpell];
     }
 
     public void setMainSpellFirst(int number) {
-        mainSpellFirst = number;
+        if (number > -1 && number < mainSpellSprites.Length)
+            mainSpellFirst = number;
+        mainSpellFirstTexture.sprite = mainSpellSprites[mainSpellFirst];
     }
 
     public void setMainSpellSecond(int number) {
-        mainSpellSecond = number;
+        if (number > -1 && number < mainSpellSprites.Length)
+            mainSpellSecond = number;
+        mainSpellSecondTexture.sprite = mainSpellSprites[mainSpellSecond];
     }
 
     public int getHealthPoints() {
@@ -82,18 +112,68 @@ public class CharacterInterface : MonoBehaviour
     {
         setHealthPoints(100);
         healthPointsText.text = "HP: " + healthPoints + "%";
+        mainSpellFirst = 0;
+        mainSpellSecond = 1;
+        healingSpell = 0;
+        parrySpell = 0;
+        mobileSpell = 0;
+        mainSpellFirstTexture.sprite = mainSpellSprites[mainSpellFirst];
+        mainSpellSecondTexture.sprite = mainSpellSprites[mainSpellSecond];
+        healingSpellTexture.sprite = healingSpellSprites[healingSpell];
+        parrySpellTexture.sprite = parrySpellSprites[parrySpell];
+        mobileSpellTexture.sprite = mobileSpellSprites[mobileSpell];
         healthPointsTexture.sprite = healthSprites[getIndexOfSprite()];
     }
 
     void Update()
     {
         //Press P to change the health points
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.M))
         {
             if (healthPoints > 0)
                 healthPoints--;
             healthPointsText.text = "HP:  " + healthPoints + "%";
             healthPointsTexture.sprite = healthSprites[getIndexOfSprite()];
+        }
+
+        //Press L to change the main spells
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            mainSpellFirst++;
+            mainSpellSecond++;
+            if (mainSpellFirst == mainSpellSprites.Length)
+                mainSpellFirst = 0;
+            if (mainSpellSecond == mainSpellSprites.Length)
+                mainSpellSecond = 0;
+            mainSpellFirstTexture.sprite = mainSpellSprites[mainSpellFirst];
+            mainSpellSecondTexture.sprite = mainSpellSprites[mainSpellSecond];
+        }
+
+        //Press P to change the parry spells
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            parrySpell++;
+            if (parrySpell == parrySpellSprites.Length)
+                parrySpell = 0;
+            parrySpellTexture.sprite = parrySpellSprites[parrySpell];
+        }
+
+        //Press K to change the mobile spells
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            mobileSpell++;
+            if (mobileSpell == mobileSpellSprites.Length)
+                mobileSpell = 0;
+            mobileSpellTexture.sprite = mobileSpellSprites[mobileSpell];
+        }
+
+        //Press O to change the healing spells
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            healingSpell++;
+            if (healingSpell == healingSpellSprites.Length)
+                healingSpell = 0;
+            healingSpellTexture.sprite = healingSpellSprites[healingSpell];
         }
     }
     
