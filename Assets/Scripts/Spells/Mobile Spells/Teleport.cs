253f8601dfed55bef10155596845a47f3a310acf
Michał Domino <micdom9@gmail.com>
2019-04-28 18:51:01 +0200

    Dodano zasięg teleportacji.

--- Assets/Scripts/Spells/Mobile Spells/Teleport.cs
+++ Assets/Scripts/Spells/Mobile Spells/Teleport.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class Teleport : InstantSpell
 {
+    public float maxRange;
+
     public override void PerformAttack(Vector3 target, bool isHostile)
     {
-        if (_attackCooldown <= 0)
+        if (_attackCooldown <= 0 && Vector3.Distance(firePoint.position, target) <= maxRange)
         {
             _attackCooldown = attackSpeed;
             firePoint.position = new Vector3(target.x, target.y, target.z);
