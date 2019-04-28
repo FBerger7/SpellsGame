b92ec92ff0fb1321dfd3a9e060aeb02aff4f2a8a
Michał Domino <micdom9@gmail.com>
2019-04-28 14:47:13 +0200

    Dodano klasę abstrakcyjną dla animacji wroga.

---
+++ Assets/Scripts/Enemies/EnemyAnimation.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public abstract class EnemyAnimation
+{
+    public void IdleAnimation(ref Animator anim)
+    {
+        if (!anim.GetBool("isIdle"))
+        {
+            anim.CrossFade("Idle", 0.1f);
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isAttack", false);
+            anim.SetBool("isDie", false);
+        }
+    }
+    public void WalkAnimation(ref Animator anim, ref NavMeshAgent agent)
+    {
+        if (!anim.GetBool("isWalk"))
+        {
+            anim.CrossFade("Walk", 0.1f);
+            anim.SetBool("isWalk", true);
+            anim.SetBool("isDie", false);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isAttack", false);
+        }
+
+    }
+
+    public void AttackAnimation(ref Animator anim, ref BaseSpell spell, Transform target)
+    {
+        if (!anim.GetBool("isAttack"))
+        {
+            anim.CrossFade("Attack", 0.1f);
+            anim.SetBool("isWalk", false);
+            anim.SetBool("isDie", false);
+            anim.SetBool("isAttack", true);
+            anim.SetBool("isIdle", false);
+        }
+
+        //Attack the target
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Attack") && anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.42f)
+            spell.PerformAttack(target.position);
+
+
+    }
+
+    public void DieAnimation(ref Animator anim)
+    {
+        anim.CrossFade("Die", 0.1f);
+        anim.SetBool("isWalk", false);
+        anim.SetBool("isDie", true);
+        anim.SetBool("isAttack", false);
+        anim.SetBool("isIdle", false);
+
+    }
+}
