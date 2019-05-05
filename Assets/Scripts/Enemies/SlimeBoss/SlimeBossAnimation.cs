c7db0b7bf16a6f656fda73d4fc818d272663cdce
Michał Domino <micdom9@gmail.com>
2019-05-05 14:21:21 +0200

    Stworzono kontroler i skrypt dla animacji bossa.

---
+++ Assets/Scripts/Enemies/SlimeBoss/SlimeBossAnimation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlimeBossAnimation : IEnemyAnimation
+{
+    public void IdleAnimation(ref Animator anim)
+    {
+        anim.CrossFade("Idle", 0.1f);
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isWalk", false);
+        anim.SetBool("isAttack", false);
+    }
+
+    public void AttackAnimation(ref Animator anim, ref BaseSpell attack, Transform target, bool isHostile)
+    {
+        // Potem
+    }
+
+    public void DieAnimation(ref Animator anim)
+    {
+        // Potem
+    }
+
+    public void WalkAnimation(ref Animator anim, ref NavMeshAgent agent)
+    {
+        // Nigdy
+    }
+}
---
+++ Assets/Scripts/Enemies/SlimeBoss/SlimeBossController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SlimeBossController : EnemyController
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookRadius = 100f;
+        maxHealth = 500f;
+
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.stoppingDistance = 30f;
+
+        _anim = GetComponent<Animator>();
+        _target = PlayerManager.instance.player.transform;
+
+        _attack = gameObject.GetComponentInChildren<SlimeBomb>();
+        _enemyAnimation = new SlimeAnimation();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
