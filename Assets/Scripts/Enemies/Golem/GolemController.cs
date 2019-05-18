6eaa62e98298f21a01e981277c37f87caceed433
Michał Domino <micdom9@gmail.com>
2019-05-18 10:57:34 +0200

    Dodano akcje do golema.

--- Assets/Scripts/Enemies/Golem/GolemController.cs
+++ Assets/Scripts/Enemies/Golem/GolemController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GolemController : EnemyController
 {
@@ -20,19 +21,41 @@ public class GolemController : EnemyController
         maxHealth = 500f;
 
         _agent = GetComponent<NavMeshAgent>();
-        lookRadius = _agent.stoppingDistance = 100f;
+        lookRadius = 100f;
+        _agent.stoppingDistance = 10f;
 
         _anim = GetComponent<Animator>();
         _target = PlayerManager.instance.player.transform;
 
         _attack = gameObject.GetComponentInChildren<SlimeBomb>();
-        _sideAttack = gameObject.GetComponentInChildren<SpawnSlime>();
+        //_sideAttack = gameObject.GetComponentInChildren<SpawnSlime>();
         _golemAnimation = new GolemAnimation();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(_target.position, transform.position);
+        if (!_anim.GetBool("isDie"))
+        {
+            if (distance <= _agent.stoppingDistance)
+            {
+                FaceTarget();
+                _golemAnimation.AttackBAnimation(ref _anim, ref _attack, _target, isHostile);
+            }
+            else if (distance <= lookRadius)
+            {
+                _agent.SetDestination(_target.position);
+                _golemAnimation.WalkAnimation(ref _anim, ref _agent);
+            }
+            else if (!_anim.GetBool("isIdle"))
+            {
+                _golemAnimation.IdleAnimation(ref _anim);
+            }
+        }
+        else if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Die") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
