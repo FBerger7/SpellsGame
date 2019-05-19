a962c976f4c9983d27357863a9f1c35c0fe47c7c
Michał Domino <micdom9@gmail.com>
2019-05-19 09:27:53 +0200

    Dodano fazy bossa.

--- Assets/Scripts/Enemies/Golem/GolemController.cs
+++ Assets/Scripts/Enemies/Golem/GolemController.cs
@@ -36,27 +36,66 @@ public class GolemController : EnemyController
     void Update()
     {
         float distance = Vector3.Distance(_target.position, transform.position);
+        if (_phaseOne) PhaseOne();
+        else if (_phaseTwo) PhaseTwo();
+        else if (_phaseThree) PhaseThree();
+
         if (!_anim.GetBool("isDie"))
         {
-            if (distance <= _agent.stoppingDistance)
-            {
-                FaceTarget();
-                _golemAnimation.AttackGAnimation(ref _anim, ref _attack, _target, isHostile);
-                //_golemAnimation.AttackBAnimation(ref _anim, ref _attack, _target, isHostile);
-            }
-            else if (distance <= lookRadius)
-            {
-                _agent.SetDestination(_target.position);
-                _golemAnimation.WalkAnimation(ref _anim, ref _agent);
-            }
-            else if (!_anim.GetBool("isIdle"))
-            {
-                _golemAnimation.IdleAnimation(ref _anim);
-            }
+            if (_phaseOne) PhaseOne();
+            else if (_phaseTwo) PhaseTwo();
+            else if (_phaseThree) PhaseThree();
+            //if (distance <= _agent.stoppingDistance)
+            //{
+            //    FaceTarget();
+            //    _golemAnimation.AttackGAnimation(ref _anim, ref _attack, _target, isHostile);
+            //    //_golemAnimation.AttackBAnimation(ref _anim, ref _attack, _target, isHostile);
+            //}
+            //else if (distance <= lookRadius)
+            //{
+            //    _agent.SetDestination(_target.position);
+            //    _golemAnimation.WalkAnimation(ref _anim, ref _agent);
+            //}
+            //else if (!_anim.GetBool("isIdle"))
+            //{
+            //    _golemAnimation.IdleAnimation(ref _anim);
+            //}
         }
         else if (_anim.GetCurrentAnimatorStateInfo(0).IsName("Die") && _anim.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void PhaseOne()
+    {
+        if (CurrentHealth < 0.6f * maxHealth)
+        {
+            Debug.Log("Phase Two started.");
+            _phaseOne = false;
+            _phaseTwo = true;
+            return;
+        }
+    }
+
+    private void PhaseTwo()
+    {
+        if (CurrentHealth < 0.3f * maxHealth)
+        {
+            Debug.Log("Phase Three started.");
+            _phaseTwo = false;
+            _phaseThree = true;
+            return;
+        }
+    }
+
+    private void PhaseThree()
+    {
+    }
+
+    public override void Die()
+    {
+        _enemyAnimation.DieAnimation(ref _anim);
+        Debug.Log(transform.name + " died");
     }
 }
